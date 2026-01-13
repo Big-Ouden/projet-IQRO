f8eabc72f1da4ffb44735c6fc080c7a5a7051c73
Simon BELIER <simon.belier@ensiie.eu>
2026-01-13 12:05:41 +0100

    tets

--- code/MaxXorSat.py
+++ code/MaxXorSat.py
@@ -1,6 +1,24 @@
-class MaxXorSat:
-    def __init__(self):
-        pass
+import numpy as np
 
-    def compute(self):
-        pass
+class MaxXorSat:
+    def __init__(self, n  , m , A, b):
+        # A taille nxm
+        if (len(A) != n):
+            print("A n'a pas n lignes");
+        if (len(A[0]) != m):
+            print("A n'a pas m colonnes")
+
+
+        # b taille nx1
+        if(len(b) != n):
+            print("b n'a pas n lignes")
+
+        self.n = n
+        self.m = m
+        self.A = A
+        seld.b = b
+
+
+#retourne la solution qui maximise et utilité = nombre d'égalité vérifié
+def solve(entry: MaxXorSat):
+
